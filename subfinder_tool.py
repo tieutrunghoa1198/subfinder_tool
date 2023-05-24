cf0df9c9ab585242144593fc3165d2292023e5d5
tieu hoa <tieutrunghoa1198@gmail.com>
2023-05-24 11:46:31 +0700

    remove unused code in subfinder_tool.py

--- subfinder_tool.py
+++ subfinder_tool.py
@@ -15,13 +15,3 @@ class SubfinderTool:
 
     def get_unique_subdomains(self):
         return self.subdomains
-
-if __name__ == '__main__':
-    # Example usage
-    domain = 'example.com'
-    tool = SubfinderTool(domain)
-    tool.enumerate_subdomains()
-    subdomains = tool.get_unique_subdomains()
-
-    # Pass the list of unique subdomains to another tool or function
-    print(subdomains)  # or use the list as needed
