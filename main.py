795e6a3bed5ad56bf39a0dd21f1ea48e04d9bec5
tieu hoa <tieutrunghoa1198@gmail.com>
2023-05-29 17:11:37 +0700

    remove amass for debugging

--- main.py
+++ main.py
@@ -5,36 +5,33 @@ from amass_tool import AmassTool
 import time
 class SubDomainReconTool:
     def __init__(self): 
-        self.domain = 'example.com'
-
+        self.domain = 'bugcrowd.com'
+        self.listDomain = []
     def run(self):
         startTime = time.time() 
 
         findomainList = FindomainTool(self.domain).enumerate_subdomains()
-        amassList = AmassTool(self.domain).enumerate_subdomains()
+        # amassList = AmassTool(self.domain).enumerate_subdomains()
         assetfinderList = AssetFinderTool(self.domain).enumerate_subdomains()
         subfinderList = SubfinderTool(self.domain).enumerate_subdomains()
-        listDomain = amassList + assetfinderList + findomainList + subfinderList
-        # listDomain = assetfinderList + findomainList + subfinderList
+        # listDomain = amassList + assetfinderList + findomainList + subfinderList
+        listDomain = assetfinderList + findomainList + subfinderList
         uniqueList = list(set(listDomain))
         print(uniqueList)
         
         print(f"[+] Script finish in: {time.time() - startTime}")
-
+        self.listDomain = uniqueList
         return uniqueList
     
-    def writeToFile(self, listDomain): 
+    def writeToFile(self): 
+        if len(self.listDomain) == 0: return
         with open("output.txt", "w") as file:
             # Iterate over each element in the list
-            for item in listDomain:
+            for item in self.listDomain:
                 # Write the element to the file
                 file.write(str(item) + "\n")
 
 if __name__ == '__main__':
-    # Example usage
-    domain = 'example.com'
     tool = SubDomainReconTool()
-    result = tool.run()
-    tool.writeToFile(result)
-    # Pass the list of unique subdomains to another tool or function
-    print(result)  # or use the list as needed
+    tool.run()
+    tool.writeToFile()
